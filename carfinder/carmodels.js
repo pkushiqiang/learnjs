ba73d1fd93810897fa1bbe3b384a15a2e6146f97
Frank Guo <pkushiqiang@gmail.com>
2015-07-04 18:47:11 -0500

    add car finder

---
+++ carfinder/carmodels.js
@@ -0,0 +1,38 @@
+var nissanModel = {"Altima":"altima",
+		               "SENTRA":"sentra"  };
+		               
+var hondaModel =  {"Accord":"accord",
+					"Civic":"civic"  };
+					
+var toyotaModel =  {"Camry":"camry",
+					"Corolla":"corolla"  };
+					
+// --------------------------------------------
+
+var modelMap = { "nissan": nissanModel, 
+				  "toyota": toyotaModel, 
+				  "honda" : hondaModel 
+			   };
+			   
+			   
+// --------------------------------------------			   
+var styleMap = {};
+styleMap["nissan-sentra"] = { "AAA": "aaa" ,
+						  "BBB":"bbb",
+						  "CCC": "vvv" }; 
+						  
+styleMap["honda-accord"] = {  "DX Sedan 4D": "dx-sedan-4d" ,								  
+							  "LX Sedan 4D": "lx-sedan-4d" ,
+							  "EX Sedan 4D": "ex-sedan-4d" ,
+							  "LX Coupe 2D": "lx-coupe-2d" ,
+							  "EX Coupe 2D": "ex-coupe-2d"  }; 
+							  
+styleMap["toyota-camry"] = {  "CE Sedan 4D": "ce-sedan-4d" ,								  
+							 "LE Sedan 4D": "le-sedan-4d" ,	
+							 "SE Sedan 4D": "se-sedan-4d" ,	
+							 "XLE Coupe 2D": "xle-coupe-4d"  }; 
+
+styleMap["nissan-sentra"] = {  "Sedan 4D": "sedan-4d" ,								  
+							 "S Sedan 4D": "s-sedan-4d" ,	
+							 "SL Sedan 4D": "sl-sedan-4d" ,	
+							 "SE-R Coupe 2D": "se-r-coupe-4d"  }; 
